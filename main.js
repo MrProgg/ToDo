4db8879a428da961036a0eb9559430efd4609b5b
MrProgg <timosko5@inbox.lv>
2019-02-20 20:17:57 +0100

    New functions, description

--- main.js
+++ main.js
@@ -13,36 +13,53 @@ $(function () {
 		var taskDescription = $('textarea').val();
 		var check = false;
 		
+// Checking for empty input
 		if (!taskName || !taskDescription) {
+			var warning = `<div id='warn'>Необходимо заполнить оба поля</div>`
+			$('body').append(warning);
+			setTimeout(function(){
+				$('#warn').remove()
+			}, 2000)
 			return false;
 		};	
 
+// Adds checking for the same task and marks it
 		$(".new-task").each(function () {		
 			if ($(this).text().trim() === taskName) {
 				$(this).addClass('alert')
 				setTimeout(function(){
 					$('.new-task').removeClass('alert')
-				}, 2000)
+				}, 2500)
 				check = true
+
+// Scrols same task to the top
+				$([document.documentElement, document.body]).animate({
+					scrollTop: $(this).offset().top -70
+			}, 500);
 			};
 		});
-		
+
+// Adds warning text to the top of the page
 		if (check) {
 			var warning = `<div id='fail'>Такое задание уже есть</div>`
 			$('body').append(warning);
 			setTimeout(function(){
 				$('#fail').remove()
-			}, 2500)
+			}, 2000)
+
+// Adds new task
 		} else {
 			hideTask();
 			var taskList = `<div class='one-task'><div class='new-task'>${taskName}<img class='delete-button' src='img/red-x.png' alt='del'><img class='description-open-button' src='img/Black-triangle.png' alt='open'></div><div class='new-task-description'>${taskDescription}</div></div>`;
 			$('#task-container').append(taskList);
 		};
 
+// Clears input aeria
 		$('input').val('');
 		$('textarea').val('');
 	});
 
+// Delete the task
 	$('#task-container').on('click', '.delete-button', function () {
 		$(this).parent().parent().remove();
 		if ($('#task-container').html().trim() === '') {
@@ -50,6 +67,7 @@ $(function () {
 		} else hideTask();
 	});
 
+// Hides or opens task description
 	$('#task-container').on('click', '.description-open-button', function () {
 		$(this).parent().next().fadeToggle();
 		$(this).toggleClass('description-open-button-toggle')
